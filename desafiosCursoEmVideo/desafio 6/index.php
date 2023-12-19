bbfb17527369473ccf855c0807c4f82ee1200953
AnaSavulski <108679110+AnaSavulski@users.noreply.github.com>
2023-12-18 21:00:26 -0300

    desafio 6
    correção

---
+++ desafiosCursoEmVideo/desafio 6/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $dividendo = $_GET['d1'] ?? 0;
+        $divisor = $_GET['d2'] ?? 1;
+    ?>
+    <main>
+        <h1>Anatomia da divisão</h1>
+        <form action="" method="get">
+            <label for="d1">Dividendo</label>
+            <input type="number" name="d1" id="d1" value="<?=$dividendo?>">
+            <label for="d2">Divisor</label>
+            <input type="number" name="d2" id="d2" value="<?=$divisor?>">
+            <input type="submit" value="Analisar">
+        </form>
+    </main>
+    <section>
+        <h2>Estrutura da divisão</h2>
+    </section>
+    
+</body>
+</html>
