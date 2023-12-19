a21af1cdc52be5262f485210d7247b9ed716b771
AnaSavulski <108679110+AnaSavulski@users.noreply.github.com>
2023-12-18 21:32:43 -0300

    calculos
    calculos

--- desafiosCursoEmVideo/desafio 6/index.php
+++ desafiosCursoEmVideo/desafio 6/index.php
@@ -15,14 +15,26 @@
         <h1>Anatomia da divisão</h1>
         <form action="" method="get">
             <label for="d1">Dividendo</label>
-            <input type="number" name="d1" id="d1" value="<?=$dividendo?>">
+            <input type="number" name="d1" id="d1" min="0" value="<?=$dividendo?>">
             <label for="d2">Divisor</label>
-            <input type="number" name="d2" id="d2" value="<?=$divisor?>">
+            <input type="number" name="d2" id="d2" min="1" value="<?=$divisor?>">
             <input type="submit" value="Analisar">
         </form>
     </main>
     <section>
         <h2>Estrutura da divisão</h2>
+        <?php 
+        //Cálculos
+        $quociente = intdiv($dividendo , $divisor);
+        $resto = $dividendo % $divisor;
+        
+            echo "<ul>";
+            echo "<li>Dividendo: $dividendo</li>";
+            echo "<li>Divisor: $divisor</li>";
+            echo "<li>Quociente: $quociente</li>";
+            echo "<li>Resto: $resto</li>";
+            echo "</ul>";
+        ?>
     </section>
     
 </body>
